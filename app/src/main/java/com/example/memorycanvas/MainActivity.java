84ab5c695b6f717a01447bd73e1180a88ba3f0cd
Anastasia Kochmar <56602236+Anastasia5353@users.noreply.github.com>
2020-05-25 22:10:56 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/memorycanvas/MainActivity.java
+++ app/src/main/java/com/example/memorycanvas/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.memorycanvas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.sax.StartElementListener;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     TilesView view;
+    Boolean start;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         view = findViewById(R.id.view);
+        button = findViewById(R.id.button);
+        start = true;
     }
 
     public void onNewGameClick(View v) {
-        view.newGame(); // запустить игру заново
+        view.newGame();
+        button.setText("Играть заново");
 
-    }
+    }// запустить игру заново
 }
+
+
+
